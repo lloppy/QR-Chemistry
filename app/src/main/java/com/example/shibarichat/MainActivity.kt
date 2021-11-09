ada264dc282953637da3cdc70e171fead2cf27a5
lloppy <ankudinovapol@gmail.com>
2021-11-09 18:07:59 +0500

    add binding, send simple messages like chat

--- app/src/main/java/com/example/shibarichat/MainActivity.kt
+++ app/src/main/java/com/example/shibarichat/MainActivity.kt
@@ -2,17 +2,43 @@ package com.example.shibarichat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shibarichat.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        binding.bSend.setOnClickListener{
+            myRef.setValue(binding.edMessage.text.toString())
+        }
+        onCangeListener(myRef)
+    }
+
+    private fun onCangeListener(dRef: DatabaseReference){
+        dRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append("Polina : ${snapshot.value.toString()}")
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 }
